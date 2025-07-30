0aed455b2412ebf46a5c43a8f46e778898d48344
chokeme <102754166+lostconsciousness@users.noreply.github.com>
2025-07-30 23:23:56 +0300

    Update main.ts

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -15,7 +15,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document)
 
-  await app.listen(3000);
+  const port = parseInt(process.env.PORT, 10) || 3000;
+  await app.listen(port, '0.0.0.0');
   console.log('API Gateway is running on http://localhost:3000');
 }
 bootstrap();
